64e8e63f2ca3f3d491c1091e208d523f7f98082d
Prabhat Singh Raghav <156128444+prabhatraghav@users.noreply.github.com>
2024-02-20 22:57:24 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 result=$( sudo docker images -q reactimage )
-docker run -itd --name My-first-container2 -p 8000:5000 $result
+docker run -itd --name My-first-container2 -p 8089:9095 $result
 CONTAINER_NAME='My-first-container2'
 x=$(docker ps -q -f status=running -f name=$CONTAINER_NAME)
 if [[ -n "$x" ]]; then
